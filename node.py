7e30d7dea7f4334d1cf863183d2ea1b241e0be61
Michael Garcia <thecrazygm@gmail.com>
2023-05-04 11:16:31 -0400

    initial commit 2

---
+++ node.py
@@ -0,0 +1,167 @@
+import json
+import logging
+import os
+import time
+
+from beem import Hive
+from beem.block import Blocks
+from beem.blockchain import Blockchain
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+)
+logger = logging.getLogger("MagnetBank Node")
+logger.setLevel(logging.DEBUG)
+
+# Load environment variables
+load_dotenv()
+
+# Connect to MongoDB
+mongo_db = os.getenv("MONGO_DB")
+client = MongoClient(mongo_db)
+db = client.torrents
+settings_collection = db.settings
+torrents_collection = db.torrents
+
+# Connect to Hive blockchain
+hive_nodes = os.getenv("HIVE_NODE")
+hive = Hive(nodes=hive_nodes)
+
+
+def get_last_block():
+    """
+    Retrieve the last block number from the database.
+
+    Returns
+    -------
+    int: The last block number.
+    """
+    # Find the last block number in the settings collection
+    last_block_data = settings_collection.find_one({"id": 1}, {"last_block": 1})
+    if last_block_data.get("last_block") is not None:
+        return last_block_data["last_block"]
+    # If the last block number is not found, use the GENISYS_BLOCK environment variable
+    last_block = int(os.getenv("GENISYS_BLOCK"))
+    set_block("last_block", last_block)
+    # Update the settings collection with the new last block number
+    settings_collection.update_one(
+        {"id": 1}, {"$set": {"genisys": last_block}}, upsert=True
+    )
+    logger.warning(f"Last block not found using root starting block {last_block}.")
+    return last_block
+
+
+def set_block(key, value):
+    """
+    Set a key-value pair in the settings collection.
+
+    Args:
+    ----
+    key (str): The key to set.
+    value (any): The value to set.
+    """
+    # Update the settings collection with the new key-value pair
+    settings_collection.update_one({"id": 1}, {"$set": {key: value}}, upsert=True)
+
+
+def process_blocks(start_block):
+    """
+    Process blocks starting from a given block number.
+
+    Args:
+    ----
+    start_block (int): The block number to start processing from.
+    """
+    # Retrieve blocks from the Hive blockchain
+    blocks = Blocks(start_block, 1000, blockchain_instance=hive)
+    logger.debug(f"{len(blocks)} blocks retrieved")
+    # Process each block
+    for block in blocks:
+        last_block = block.block_num
+        # Check if the block contains a MagnetBank custom JSON operation
+        for operation in block.operations:
+            if (
+                operation["type"] == "custom_json_operation"
+                and operation["value"]["id"] == "MagnetBank"
+            ):
+                # Extract data from the custom JSON operation
+                time = block["timestamp"].isoformat()
+                number = block.block_num
+                submitted_by = operation["value"]["required_posting_auths"][0]
+                json_data = json.loads(operation["value"]["json"])
+                # Check if the data is well-formed
+                if all(
+                    key in json_data
+                    for key in ["hash", "file_name", "category", "announce_url"]
+                ):
+                    # Add additional data to the JSON object
+                    json_data.update(
+                        {
+                            "timestamp": time,
+                            "submitted_by": submitted_by,
+                            "block_number": number,
+                        }
+                    )
+                    # Check if the hash key already exists in the database
+                    if (
+                        torrents_collection.find_one({"hash": json_data["hash"]})
+                        is None
+                    ):
+                        # If the hash key does not exist, add the JSON object to the database
+                        logger.info(
+                            f"Hash key {json_data['hash']} added to database - {json_data['file_name']} by {json_data['submitted_by']}"
+                        )
+                        torrents_collection.insert_one(json_data)
+                    else:
+                        # If the hash key already exists, log a warning
+                        logger.warning(
+                            f"Hash key {json_data['hash']} already exists in the database."
+                        )
+                else:
+                    # If the data is not well-formed, log a warning
+                    logger.warning("Malformed data received.")
+            # Update the last block number in the settings collection
+            set_block("last_block", last_block)
+
+
+def main():
+    """
+    Run the MagnetBank Node.
+    """
+    while True:
+        try:
+            # Get the current block number from the Hive blockchain
+            head_block = Blockchain(blockchain_instance=hive).get_current_block_num()
+            # Update the head block number in the settings collection
+            set_block("head_block", head_block)
+            # Get the last block number from the settings collection
+            last_block = get_last_block()
+            # Calculate the difference between the head block and the last block
+            difference = head_block - last_block
+            logger.debug(
+                f"Current block: {head_block} Last block: {last_block} Difference: {difference}"
+            )
+            # Process blocks in batches of 1000
+            rounds = (difference // 1000) + 1
+            for r in range(rounds):
+                logger.debug(
+                    f"Round {r+1}/{rounds} from block {last_block+1} to block {min(last_block+1000, head_block)}"
+                )
+                start_block = last_block + 1
+                process_blocks(start_block)
+                last_block = min(last_block + 1000, head_block)
+            # Wait for the specified amount of time before processing the next batch of blocks
+            sleep_time = float(os.getenv("SLEEP_TIME"))
+            time.sleep(sleep_time)
+        except Exception as e:
+            # Log any errors that occur
+            logger.error(e)
+
+
+if __name__ == "__main__":
+    main()
